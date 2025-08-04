cd1a15f16a85eebebe9df4a48c8ab16077a3ad2f
PaulaV09 <pviviescas@unab.edu.co>
2025-08-04 15:17:03 -0500

    feat: :sparkles: Add ingrediente
    Implementacion de la funcionalidad agregar ingrediente

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Autor: Paula Andrea Viviescas Jaimes 
+Fecha: 04/08/2025
+Descripción: Sistema de gestion de inventario de una hamburgueseria
+"""
+
+import app.mainmenu as mainmenu
+
+if __name__ == "__main__":
+        mainmenu.prueba()
